a820a81e3bd875fb32d879907e4527021b6627ed
morris17935 <30794944+morris17935@users.noreply.github.com>
2017-10-01 00:47:33 +0800

    change

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <gtest/gtest.h>
-#include "utVariable.h"
+#include "utSort.h"
 
 int main( int argc , char **argv )
 {
---
+++ utSort.h
@@ -0,0 +1,112 @@
+#ifndef EXP_H
+#define EXP_H
+#include "include/Shapes.h"
+#include "include/Media.h"
+#include "include/Sort.h"
+using namespace std;
+TEST (Sort, sortByIncreasingPerimeter) {
+	std::list<Shape *> test;
+	Sort fortest;
+	vertex a,b,c;
+	a.x = 1;
+	a.y = 2;
+	b.x = 4;
+	b.y = 2;
+	c.x = 4;
+	c.y = 6;
+	Shape *temp1 = new Rectangle(1, 1, 2, 2, "rectangle"); 
+	Shape *temp2 = new Circle(1, 1, 2, "circle");
+	Shape *temp3 = new Triangle(a, b, c, "triangle");
+	test.push_back(temp1);
+	test.push_back(temp2);
+	test.push_back(temp3);
+	fortest.sortByIncreasingPerimeter(&test);
+	std::list<Shape *>::iterator itertest = test.begin();
+	ASSERT_EQ(8, (*(*itertest)).perimeter());
+	itertest++;
+	ASSERT_EQ(12,(*(*itertest)).perimeter());
+	itertest++;
+	ASSERT_EQ(4* M_PI, (*(*itertest)).perimeter());
+}
+
+TEST (Sort, sortByDecreasingPerimeter) {
+	std::list<Shape *> test;
+	Sort fortest;
+	vertex a, b, c;
+	a.x = 1;
+	a.y = 2;
+	b.x = 4;
+	b.y = 2;
+	c.x = 4;
+	c.y = 6;
+	Shape *temp1 = new Rectangle(1, 1, 2, 2, "rectangle");
+	Shape *temp2 = new Circle(1, 1, 2, "circle");
+	Shape *temp3 = new Triangle(a, b, c, "triangle");
+	test.push_back(temp1);
+	test.push_back(temp2);
+	test.push_back(temp3);
+	fortest.sortByDecreasingPerimeter(&test);
+	std::list<Shape *>::iterator itertest = test.begin();
+	ASSERT_EQ(4 * M_PI, (*(*itertest)).perimeter());
+	itertest++;
+	ASSERT_EQ(12, (*(*itertest)).perimeter());
+	itertest++;
+	ASSERT_EQ(8, (*(*itertest)).perimeter());
+}
+
+TEST (Sort, sortByIncreasingArea) {
+	std::list<Shape *> test;
+	Sort fortest;
+	vertex a, b, c;
+	a.x = 1;
+	a.y = 2;
+	b.x = 4;
+	b.y = 2;
+	c.x = 4;
+	c.y = 6;
+	Shape *temp1 = new Rectangle(1, 1, 2, 2, "rectangle");
+	Shape *temp2 = new Circle(1, 1, 2, "circle");
+	Shape *temp3 = new Triangle(a, b, c, "triangle");
+	test.push_back(temp1);
+	test.push_back(temp2);
+	test.push_back(temp3);
+	fortest.sortByIncreasingArea(&test);
+	std::list<Shape *>::iterator itertest = test.begin();
+	ASSERT_EQ(4, (*(*itertest)).area());
+	itertest++;
+	ASSERT_EQ(6, (*(*itertest)).area());
+	itertest++;
+	ASSERT_EQ(4 * M_PI, (*(*itertest)).area());
+}
+
+TEST (Sort, sortByDecreasingArea) {
+	std::list<Shape *> test;
+	Sort fortest;
+	vertex a, b, c;
+	a.x = 1;
+	a.y = 2;
+	b.x = 4;
+	b.y = 2;
+	c.x = 4;
+	c.y = 6;
+	Shape *temp1 = new Rectangle(1, 1, 2, 2, "rectangle");
+	Shape *temp2 = new Circle(1, 1, 2, "circle");
+	Shape *temp3 = new Triangle(a, b, c, "triangle");
+	test.push_back(temp1);
+	test.push_back(temp2);
+	test.push_back(temp3);
+	fortest.sortByDecreasingArea(&test);
+	std::list<Shape *>::iterator itertest = test.begin();
+	ASSERT_EQ(4 * M_PI, (*(*itertest)).area());
+	itertest++;
+	ASSERT_EQ(6, (*(*itertest)).area());
+	itertest++;
+	ASSERT_EQ(4, (*(*itertest)).area());
+}
+
+
+TEST (Sort, sortByIncreasingCompactness) {
+
+}
+
+#endif
